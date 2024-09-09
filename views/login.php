572d80b6bee68025768ae3a259350abe16b8cc24
Yusrina55 <yusrinaaa05@gmail.com>
2024-09-09 13:01:09 +0700

    add dashboard-pembeli

--- views/login.php
+++ views/login.php
@@ -1,11 +1,10 @@
-<!-- src/views/login.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="./style.css" rel="stylesheet">
+    <link href="../style.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <form action="/login" method="POST" class="bg-white p-6 rounded shadow-md w-full max-w-sm">
